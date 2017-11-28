1d4178bd538ae76a149eadc9f28e81ab3380606c
nmusaelian-rally <nmusaelian@rallydev.com>
2017-11-27 20:58:04 -0700

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
+import re
 from flask import Flask, g, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_ , and_
+from sqlalchemy import or_ , and_ , func
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, Field, widgets
 
@@ -8,14 +10,13 @@ from wtforms import StringField, TextAreaField, Field, widgets
 app = Flask(__name__)
 # eventually set to os.environ['DATABASE_URL']:
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/corpus2'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['WHOOSH_BASE'] = 'woosh'
 db = SQLAlchemy(app)
 
 
-class Fragment(db.Model):
-    __tablename__ = 'fragments'
+class Thing(db.Model):
+    __tablename__ = 'things'
     __searchable__ = ['text','tags']
 
     id     = db.Column(db.Integer, primary_key=True)
@@ -45,20 +46,18 @@ class TagListField(Field):
         else:
             self.data = []
 
-class FragmentForm(FlaskForm):
+class ThingForm(FlaskForm):
     title = StringField('title')
     text  = TextAreaField('text')
     link = StringField('link')
     ref = StringField('ref')
     tags = TagListField('tags')
-    #tags = FieldList(StringField('tags'))
 
-
-# @app.route('/')
-# def index():
-#     fragments = Fragment.query.all()
-#     return render_template('index.html', fragments=fragments)
-
+def stripSpaceAndLowerTags(tags):
+    tags = tags.lower()
+    tags = re.sub(r'\s*,\s*', ',', tags).strip()
+    tags = re.sub('\s+', ' ', tags)
+    return tags
 
 @app.route('/')
 def index():
@@ -66,62 +65,70 @@ def index():
 
 @app.route('/pages/<int:page>',methods=['GET'])
 def view(page=1):
-    per_page = 2
-    fragments = Fragment.query.order_by(Fragment.id.desc()).paginate(page,per_page,error_out=True)
-    return render_template('view.html',fragments=fragments)
+    per_page = 5
+    things = Thing.query.order_by(Thing.id.desc()).paginate(page,per_page,error_out=True)
+    return render_template('view.html',things=things)
 
 
 @app.route('/pages/search')
 def search():
-    fragments = ''
+    things = ''
     if request.args.get('textQuery'):
-        fragments = Fragment.query.filter(Fragment.text.like("%{}%".format(request.args.get('textQuery')))).all()
+        txt = "%{}%".format(request.args.get('textQuery'))
+        things = Thing.query.filter(Thing.text.ilike(txt)).all()
     if request.args.get('tagsQuery'):
-        selected_tags = (request.args.get('tagsQuery')).split(",")
-        query = db.session.query(Fragment)
-        clauses = [Fragment.tags.any(tag) for tag in selected_tags]
+        tags_arr = (request.args.get('tagsQuery')).split(",")
+        tags_str = stripSpaceAndLowerTags(','.join(tags_arr))
+        tags_arr = tags_str.split(',')
+        query = db.session.query(Thing)
+        clauses = [Thing.tags.any(tag) for tag in tags_arr]
         if request.args.get('operator'):
-            fragments = query.filter(and_(*clauses)).all()
+            things = query.filter(and_(*clauses)).all()
         else:
-            fragments = query.filter(or_(*clauses)).all()
-    return render_template('results.html', fragments=fragments)
+            things = query.filter(or_(*clauses)).all()
+    return render_template('results.html', things=things)
 
 
 @app.route('/<int:id>')
 def show(id):
-    fragment = Fragment.query.get(id)
-    return render_template('show.html', fragment=fragment, id=id)
+    thing = Thing.query.get(id)
+    return render_template('show.html', thing=thing, id=id)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        fragment = Fragment(request.form['title'], request.form['text'],request.form['link'],request.form['tags'])
-        db.session.add(fragment)
+        thing = Thing(request.form['title'],
+                            request.form['text'],
+                            request.form['link'],
+                            stripSpaceAndLowerTags(request.form['tags']))
+        db.session.add(thing)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('add.html')
 
 @app.route('/edit/<int:id>', methods=['GET','POST'])
 def edit(id):
-    fragment = Fragment.query.get(id)
-    form = FragmentForm(obj=fragment)
+    thing = Thing.query.get(id)
+    form = ThingForm(obj=thing)
     if request.method == 'POST':
-        form.populate_obj(fragment)
-        db.session.add(fragment)
+        form.populate_obj(thing)
+        tags = stripSpaceAndLowerTags(','.join(thing.tags))
+        thing.tags = tags.split(',')
+        db.session.add(thing)
         db.session.commit()
-        print (fragment.id)
+        print (thing.id)
         return redirect(url_for('index'))
-    return render_template('edit.html', fragment=fragment, id=id, form=form)
+    return render_template('edit.html', thing=thing, id=id, form=form)
 
 @app.route('/delete/<int:id>', methods=['GET','POST'])
 def delete(id):
-    fragment = Fragment.query.get(id)
+    thing = Thing.query.get(id)
     if request.method == 'POST':
-        title = fragment.title
-        db.session.delete(fragment)
+        title = thing.title
+        db.session.delete(thing)
         db.session.commit()
         return redirect(url_for('confirm', title=title))
-    return render_template('delete.html', fragment=fragment, id=id)
+    return render_template('delete.html', thing=thing, id=id)
 
 @app.route('/confirm')
 def confirm():
